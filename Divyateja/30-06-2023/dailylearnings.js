6df38f2c0c276f2a7dea734eb9f41cae75e089b9
Divyateja Singampalli <99350921+Tejha93@users.noreply.github.com>
2023-06-30 17:18:07 +0530

    daily learnings and js practice

---
+++ Divyateja/30-06-2023/dailylearnings.js
@@ -0,0 +1,49 @@
+// Declaring a varibles using Var, Const, let 
+let a = 12;
+console.log(a)
+
+// By using let keywod we can declare a variable only at once 
+
+
+var n = 16;
+console.log(n) 
+
+// By using var keyword we can redeclare a variable and we can update the value of the variable 
+
+
+let t = 99;
+console.log(t)
+
+
+
+//conditional statements 
+
+var b = 99;
+
+if (b<15){
+ console.log('b is less than 15');
+}
+else if (b>15){
+    console.log("b is greater than 15");
+}
+
+else{
+    console.log("b is equal to fifteen");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
