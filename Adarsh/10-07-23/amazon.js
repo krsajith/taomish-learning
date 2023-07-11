ba7bfb12fea8925e5059e5878c61d548e390a65d
Adarsh-Ramachandran <137146945+Adarsh-Ramachandran@users.noreply.github.com>
2023-07-11 10:39:00 +0530

    Add files via upload

---
+++ Adarsh/10-07-23/amazon.js
@@ -0,0 +1,10 @@
+
+function redirectOne() {
+
+window.open('https://www.forestessentialsindia.com/travel-size-after-bath-oil-mashobra-honey-vanilla.html?msclkid=f882f01d091a1ac0598b94b6f111a53f&utm_source=bing&utm_medium=cpc&utm_campaign=MCSP_030_FE_BingAds_shopping_Purchase_Always_On_INDIA&utm_term=4588330742736683&utm_content=MCSP_030_A01-All-Products')
+
+}
+
+function redirectTwo() {
+    window.open('https://www.forestessentialsindia.com/intense-perfume-amrita.html')
+}
