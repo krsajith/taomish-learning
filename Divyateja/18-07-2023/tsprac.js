ffe2eecaa4ce14e42c001a3454d271664582765f
Divyateja Singampalli <99350921+Tejha93@users.noreply.github.com>
2023-07-19 10:06:22 +0530

    Add files via upload

---
+++ Divyateja/18-07-2023/tsprac.js
@@ -0,0 +1,19 @@
+"use strict";
+//fuctions
+Object.defineProperty(exports, "__esModule", { value: true });
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3));
+
+
+//arrow functions 
+var a = function (n, m) {
+    return n + m;
+};
+console.log(a(5, 3));
+
+
+
+
+
