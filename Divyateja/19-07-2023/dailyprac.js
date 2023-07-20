d672fac92f1d3cb91616f32a332e63567f37f7b6
Divyateja Singampalli <99350921+Tejha93@users.noreply.github.com>
2023-07-20 12:02:52 +0530

    typescript

---
+++ Divyateja/19-07-2023/dailyprac.js
@@ -0,0 +1,17 @@
+"use strict";
+//Union 
+Object.defineProperty(exports, "__esModule", { value: true });
+var scorecard = 22; //You can assign both number and string as a value to the variable 
+function scorecard2(score) {
+    if (score === 22) {
+        console.log("yes you can do");
+    }
+    else {
+        console.log("Not Allowed");
+    }
+}
+scorecard2(22);
+//Another usecase 
+var myArrow = [];
+myArrow.push(226, true, "Teja");
+console.log(myArrow);
