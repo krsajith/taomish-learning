1d23f8ebf036c714bae203935b349eff5a0acc6d
Divyateja Singampalli <99350921+Tejha93@users.noreply.github.com>
2023-07-20 12:05:14 +0530

    typescript

---
+++ Divyateja/20-07-2023/dailyprac.js
@@ -0,0 +1,20 @@
+"use strict";
+//Tuple 
+Object.defineProperty(exports, "__esModule", { value: true });
+var User;
+User = [12, "Teja", true];
+console.log(User);
+var rgb = [296, 123, 189];
+console.log(rgb);
+//Objects by using type annotations 
+var person;
+person = { name: "Teja", age: 22, city: "Kakinada" };
+console.log(person);
+var person_det = { name: "Tejaa", age: 22, city: "Siripuram" };
+console.log(person_det);
+;
+var detail = { name: "Teja", age: 23 };
+console.log(detail);
+var name;
+name = 22;
+console.log(name);
