b60750beb172666455c45e6bcd22ec56b6749e70
Divyateja Singampalli <99350921+Tejha93@users.noreply.github.com>
2023-07-26 15:04:54 +0530

    task modified

---
+++ Divyateja/task v-2/designfigma.js
@@ -0,0 +1,56 @@
+//Input Number circle v1
+
+let counterValue = 0;
+
+function updateCounter(elementId) {
+  const counterElement = document.getElementById(elementId);
+  counterElement.textContent = counterValue.toString();
+}
+
+function increase(elementId) {
+  counterValue++;
+  updateCounter(elementId);
+}
+
+function decrease(elementId) {
+  counterValue--;
+  updateCounter(elementId);
+}
+
+updateCounter("counterValue1");
+
+//Input Number circle v1 (disabled)
+
+function disable() {
+  let numb = document.getElementById("counterValue_0");
+  numb.style.color = "grey";
+  document.getElementById("disable").disabled = true;
+}
+
+//Toogle Label Highlight
+
+function highlight() {
+  let text_1 = document.getElementById("txt_1");
+  let text_2 = document.getElementById("txt_2");
+  let toogle_btn = document.getElementById("btn");
+  if (toogle_btn.checked) {
+    text_2.style.fontWeight = "700";
+    text_1.style.fontWeight = "500";
+  } else {
+    text_1.style.fontWeight = "700";
+    text_2.style.fontWeight = "500";
+  }
+}
+
+function highlight_2() {
+  let text_1 = document.getElementById("txt_3");
+  let text_2 = document.getElementById("txt_4");
+  let toogle_btn = document.getElementById("btn_2");
+  if (toogle_btn.checked) {
+    text_2.style.fontWeight = "700";
+    text_1.style.fontWeight = "500";
+  } else {
+    text_1.style.fontWeight = "700";
+    text_2.style.fontWeight = "500";
+  }
+}
